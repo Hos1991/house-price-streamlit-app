0b13fd6f3239877fb4dc65ae4de03e692068d827
Hos1991 <86062025+Hos1991@users.noreply.github.com>
2026-05-10 03:24:04 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+model = joblib.load("house_price_model.pkl")
+
+st.title("House Price Prediction App")
+
+overall_qual = st.slider("Overall Quality", 1, 10, 5)
+gr_liv_area = st.number_input("Above Ground Living Area", min_value=0)
+total_bsmt_sf = st.number_input("Total Basement Area", min_value=0)
+age = st.number_input("House Age", min_value=0)
+lot_area = st.number_input("Lot Area", min_value=0)
+garage_area = st.number_input("Garage Area", min_value=0)
+garage_cars = st.slider("Garage Cars", 0, 5, 1)
+tot_rooms = st.slider("Total Rooms Above Ground", 1, 15, 6)
+full_bath = st.slider("Full Bathrooms", 0, 5, 1)
+
+lot_area_log = np.log(lot_area) if lot_area > 0 else 0
+
+input_data = pd.DataFrame([{
+    "OverallQual": overall_qual,
+    "GrLivArea": gr_liv_area,
+    "TotalBsmtSF": total_bsmt_sf,
+    "Age": age,
+    "LotArea_log": lot_area_log,
+    "GarageArea": garage_area,
+    "GarageCars": garage_cars,
+    "TotRmsAbvGrd": tot_rooms,
+    "FullBath": full_bath
+}])
+
+if st.button("Predict Price"):
+    prediction = model.predict(input_data)[0]
+    st.success(f"Predicted House Price: ${prediction:,.0f}")
